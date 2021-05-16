0340b72d9ae8fddd3e8f93ac11ac5ca6d4e156f0
Phoebe Zizirou <zizirou.p@live.unic.ac.cy>
2021-05-16 20:15:15 +0300

    Lab 5

--- UnitTestForLab4/UnitTestForLab4.cpp
+++ UnitTestForLab4/UnitTestForLab4.cpp
@@ -1,5 +1,11 @@
+/**
+* @TODO
+* Details Unit tests for the matrix library...
+*/
+
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../Tutorial4Part2/Matrix.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -8,9 +14,25 @@ namespace UnitTestForLab4
 	TEST_CLASS(UnitTestForLab4)
 	{
 	public:
+
+		//Testing sumOfRows
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestMethodForsumOfRow)
 		{
+			//Arrange
+
+			const int MAX_ROW = 5;
+			double matrix[MAX_ROW][MAX_COL] = { 1 , 1, 1 };
+
+			//Act
+
+			double sum0 = sumOfRow(matrix, 0, MAX_ROW);
+			double sum1 = sumOfRow(matrix, 1, MAX_ROW);
+
+			//Assert
+
+			Assert::AreEqual(3.0, sum0);
+			Assert::AreEqual(0.0, sum1);
 		}
 	};
 }
