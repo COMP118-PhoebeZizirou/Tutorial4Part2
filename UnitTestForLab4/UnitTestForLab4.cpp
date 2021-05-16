d437c3e60d8e639742db310d35bd468841ef7133
Phoebe Zizirou <zizirou.p@live.unic.ac.cy>
2021-05-16 19:50:14 +0300

    Skeleton for Unit Testing

---
+++ UnitTestForLab4/UnitTestForLab4.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestForLab4
+{
+	TEST_CLASS(UnitTestForLab4)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
